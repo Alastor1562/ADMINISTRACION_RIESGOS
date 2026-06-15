c5582b0534e46808902616e73747c47358a10cb3
Samuel Gomez Jimenez <samuelgomezjimenez2@gmail.com>
2026-06-14 19:55:42 -0600

    FINAL DE SEMESTRE

--- VP_Bonos.m
+++ VP_Bonos.m
@@ -1,9 +1,9 @@
 clear all; close all; clc;
 
-yield = 0.0864;
-cupon = 0.08;
-valuation_day = '27-feb-2026';
-expiration_day = '21-feb-2036';
+yield = 0.0824;
+cupon = 0.085;
+valuation_day = '13-may-2026';
+expiration_day = '31-may-2029';
 
 price = bndprice(yield, cupon, valuation_day, expiration_day, 'Basis',2) 
 PV01 = bndprice(yield - 0.0001, cupon, valuation_day, expiration_day, 'Basis',2)
